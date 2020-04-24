0fd08a9f2abde5e71221d036aa47811d9221a045
Andrew Schwartz <andrew@mk-dir.com>
2020-04-23 20:46:59 -0400

    run styler

--- formula_for_cal.R
+++ formula_for_cal.R
@@ -4,14 +4,14 @@ library(openxlsx)
 
 
 # step1: patients and bed needs  input----
-n_covid_pt = 140
-#icu
-n_pt_icu = 40
-n_pt_icu_vent = 10
+n_covid_pt <- 140
+# icu
+n_pt_icu <- 40
+n_pt_icu_vent <- 10
 
 
-n_pt_non_icu = n_covid_pt - n_pt_icu
-n_pt_icu_non_vent = n_pt_icu - n_pt_icu_vent
+n_pt_non_icu <- n_covid_pt - n_pt_icu
+n_pt_icu_non_vent <- n_pt_icu - n_pt_icu_vent
 
 
 # # read team ratio from google-----
@@ -27,40 +27,42 @@ n_pt_icu_non_vent = n_pt_icu - n_pt_icu_vent
 #  saveRDS(team_ratio, file = "../covid_staff_calculator/data/team_ratio.rds")
 
 # step2: load ratio ------
-team = read_rds("data/team_ratio.rds")
+team <- read_rds("data/team_ratio.rds")
 
-roles = unique(team$role)
+roles <- unique(team$role)
 
 
 # step3 assign staff -----
 # ICU ------
 # icu team ratio
-icu_ratio = team_ratio %>%
+icu_ratio <- team_ratio %>%
   filter(team_tpye == "ICU")
 
-staff_icu = icu_ratio %>%
+staff_icu <- icu_ratio %>%
   transmute(role,
-            n_staff = ceiling(n_icu_pt/n_bed_per_person),
-            n_staff_strech = ceiling(n_icu_pt/n_bed_per_person_stretch))
+    n_staff = ceiling(n_icu_pt / n_bed_per_person),
+    n_staff_strech = ceiling(n_icu_pt / n_bed_per_person_stretch)
+  )
 
 
 # General --------
-n_pt_covid_gen = n_covid_pt*covid_general_ratio
+n_pt_covid_gen <- n_covid_pt * covid_general_ratio
 
 # non icu team ratio
-gen_ratio = team_ratio %>%
+gen_ratio <- team_ratio %>%
   filter(team_tpye == "General")
 
-staff_gen = gen_ratio %>%
+staff_gen <- gen_ratio %>%
   transmute(role,
-            n_staff = ceiling(n_pt_covid_gen/n_bed_per_person),
-            n_staff_strech = ceiling(n_pt_covid_gen/n_bed_per_person_stretch))
+    n_staff = ceiling(n_pt_covid_gen / n_bed_per_person),
+    n_staff_strech = ceiling(n_pt_covid_gen / n_bed_per_person_stretch)
+  )
 
 
 
 full_join(staff_gen, staff_icu, by = "role") %>%
-   mutate_if(is.numeric, as.integer)  %>%
-   mutate_if(is.numeric, function(x) ifelse(is.infinite(x), NA, x)) %>%
-   tidyext::row_sums(n_staff, n_staff_needed, varname = "n_normal", na_rm = T) %>%
-   tidyext::row_sums(n_staff_strech, n_person_strech_icu, varname = "n_strech", na_rm = T) %>%
-   select(role, n_normal, n_strech)
+  mutate_if(is.numeric, as.integer) %>%
+  mutate_if(is.numeric, function(x) ifelse(is.infinite(x), NA, x)) %>%
+  tidyext::row_sums(n_staff, n_staff_needed, varname = "n_normal", na_rm = T) %>%
+  tidyext::row_sums(n_staff_strech, n_person_strech_icu, varname = "n_strech", na_rm = T) %>%
+  select(role, n_normal, n_strech)
